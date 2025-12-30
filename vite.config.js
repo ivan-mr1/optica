c9e25ed1a2d1dbd3c9f48a8d2d881d5f07f3f65d
ivan-mr1 <ivanitit3535@gmail.com>
2025-12-30 17:39:46 +0200

    fix: vite config

--- vite.config.js
+++ vite.config.js
@@ -1,71 +1,20 @@
 import { defineConfig } from 'vite';
-import path from 'path';
-import { sync } from 'glob';
 import SassGlob from 'vite-plugin-sass-glob-import';
-import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
-import { createSvgIconsPlugin } from 'vite-plugin-svg-icons';
+import { svgSpritePlugin } from './vite-plugins/svg-sprite';
+import { imageOptimizerPlugin } from './vite-plugins/image-optimizer';
+import { buildConfig } from './vite-plugins/build-config';
 
 export default defineConfig({
-  plugins: [
-    SassGlob(),
-    // Конфигурация SVG спрайта
-    createSvgIconsPlugin({
-      // Путь к папке с иконками
-      iconDirs: [path.resolve(process.cwd(), 'src/assets/icons')],
-      // Формат ID: icon-monochrome-name или icon-colored-name
-      symbolId: 'icon-[dir]-[name]',
-      svgoOptions: {
-        full: true, // Отключаем все дефолтные плагины SVGO, чтобы они не меняли цвета сами по себе
-        plugins: [
-          {
-            name: 'removeAttrs',
-            params: {
-              attrs: '(fill|stroke)',
-            },
-            // Логика: если файл в папке 'colored', мы возвращаем null,
-            // тем самым отменяя удаление атрибутов для этого файла
-            fn: (item, params, info) => {
-              if (info.path && info.path.includes('colored')) {
-                return null;
-              }
-            },
-          },
-          // Базовые безопасные оптимизации для всех иконок
-          'removeXMLNS',
-          'removeDimensions',
-          'removeViewBox', // Опционально, если хотите оставить viewBox, удалите эту строку
-        ],
-      },
-    }),
-    ViteImageOptimizer({
-      png: { quality: 70 },
-      jpeg: { quality: 70 },
-      jpg: { quality: 70 },
-      webp: { quality: 85 },
-      avif: { lossless: true },
-      // Исключаем svg из оптимизатора, так как мы настроили его выше в createSvgIconsPlugin
-      svg: false,
-    }),
-  ],
-  build: {
-    rollupOptions: {
-      input: sync('src/**/*.html'.replace(/\\/g, '/')),
-      output: {
-        assetFileNames: (assetInfo) => {
-          let extType = assetInfo.name;
-          if (/css/.test(extType)) {
-            extType = 'assets/css';
-          }
-          return assetInfo.originalFileName ?? `${extType}/[name][extname]`;
-        },
-        chunkFileNames: 'assets/js/[name].js',
-        entryFileNames: 'assets/js/[name].js',
-      },
-    },
-    assetsInlineLimit: 0,
-    emptyOutDir: true,
-    outDir: '../dist',
-  },
   root: 'src',
   base: '',
+
+  plugins: [SassGlob(), svgSpritePlugin(), imageOptimizerPlugin()],
+
+  build: buildConfig,
+
+  server: {
+    host: true,
+    port: 3000,
+    open: true,
+  },
 });
