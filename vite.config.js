463804be68314e061b94c13996f025f907972a39
ivan-mr1 <ivanitit3535@gmail.com>
2026-01-14 15:00:22 +0200

    feat: add version not postHTML

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,30 @@
-import { defineConfig } from 'vite';
 import SassGlob from 'vite-plugin-sass-glob-import';
+import { defineConfig } from 'vite';
+import { sync } from 'glob';
 import { imageOptimizerPlugin } from './vite-plugins/image-optimizer';
-import { buildConfig } from './vite-plugins/build-config';
-import { htmlPlugins } from './vite-plugins/posthtml/html.js';
+import { removeAttributes } from './vite-plugins/removeAttributes';
 
 export default defineConfig({
+  plugins: [SassGlob(), imageOptimizerPlugin(), removeAttributes()],
+  build: {
+    rollupOptions: {
+      input: sync('src/**/*.html'.replace(/\\/g, '/')),
+      output: {
+        assetFileNames: (assetInfo) => {
+          let extType = assetInfo.name;
+          if (/css/.test(extType)) {
+            extType = 'assets/css';
+          }
+          return assetInfo.originalFileName ?? `${extType}/[name][extname]`;
+        },
+        chunkFileNames: 'assets/js/[name].js',
+        entryFileNames: 'assets/js/[name].js',
+      },
+    },
+    assetsInlineLimit: 0,
+    emptyOutDir: true,
+    outDir: '../dist',
+  },
   root: 'src',
   base: '',
-  plugins: [...htmlPlugins, SassGlob(), imageOptimizerPlugin()],
-  build: buildConfig,
 });
